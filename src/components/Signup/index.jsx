ac969077845d77657ce18c15637b694f1e45e50e
Bernardo <bernardocardosodacosta@gmail.com>
2021-12-08 16:08:14 -0300

    fix: change to react-modal

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,42 +1,32 @@
-import { Popover, TextField } from '@mui/material';
-import { useState } from 'react';
-import { CenterButton, Content, Container } from './styles';
+import { Button, TextField } from '@mui/material';
+import { Content } from './styles';
+import ReactModal from 'react-modal';
 
 const Signup = () => {
-    
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
+
+    const CustomStyles = {
+        content: {
+            width: "310px",
+            height: "510px",
+            margin: "0 auto",
+        }
+    }
     
     return (
         <>
-        <CenterButton variant="contained" onClick={(event) => {
-          setAnchorEl(event.currentTarget);
-        }}>cadastrar</CenterButton>
-        <Container>
-        <Popover
-            id={open ? "simple-popover" : undefined}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={() => {setAnchorEl(null)}}
-            anchorOrigin={{
-            vertical: 'center',
-            horizontal: 'center',
-            }}
-            transformOrigin={{
-              vertical: 'center',
-              horizontal: 'center',
-            }}
+        <ReactModal
+            isOpen={true}
+            style={CustomStyles}
         >
         <Content>
         <form>
             <TextField id="outlined-basic" label="Username" variant="outlined" margin="normal" />
             <TextField id="outlined-basic" label="E-mail" variant="outlined" margin="normal" />
             <TextField id="outlined-basic" label="Senha" variant="outlined" margin="normal" />
-            <button type="submit">Cadastrar</button>
+            <Button type="submit">Cadastrar</Button>
         </form>
         </Content>
-        </Popover>
-        </Container>
+        </ReactModal>
         </>
     )
 }
--- src/components/Signup/styles.js
+++ src/components/Signup/styles.js
@@ -1,21 +1,17 @@
 import styled from "styled-components"
 
-export const CenterButton = styled.button`
-    background-color: black;
-    margin: 0 auto;
-    display: flex;
-`
-export const Container = styled.div`
-    background-color: var(--cor-fundo-popover);
-`
 export const Content = styled.div`
-    width: 310px;
-    height: 511px;
-    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
     form {
         display: flex;
         flex-direction: column;
-        margin: 5rem 1rem;
+        margin: 2.5rem 1rem;
+    }
+    Button {
+        margin: 3rem 1rem;
     }
 
 `
