26d24ec75294d2108b96ab440a208644ff1a7507
murillotlopes <murillo.tlopes@hotmail.com>
2021-12-09 11:13:31 -0300

    feat(globalstyle index.html): create new colors and change the design language

--- src/style/global.js
+++ src/style/global.js
@@ -24,6 +24,7 @@ export default createGlobalStyle`
         --color-inter: linear-gradient(145deg, rgba(244,123,49,1) 0%, rgba(221,91,12,1) 89%);
         --color-santander: linear-gradient(145deg, rgba(237,39,15,1) 0%, rgba(180,31,13,1) 89%);
         --color-green: #6CD397;
+        --color-black: #3D3D3D;
         --color-font: #f4f4f4;
         --radius: 8px;
     }
