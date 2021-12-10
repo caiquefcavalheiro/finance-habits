ff3059f3050d88ca4f03343fd5dc6fe46f1fd095
murillotlopes <murillo.tlopes@hotmail.com>
2021-12-09 22:35:41 -0300

    fix: merge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-import GlobalStyle from "./style/global";
-=======
 import GlobalStyle from './style/global'
 import Routes from './routes';
->>>>>>> 45c1d50556764711f22db38a4772617686de6d73
 
 function App() {
 
 
   return (
     <>
-<<<<<<< HEAD
-      <GlobalStyle />
-=======
       <GlobalStyle/>
       <Routes />
->>>>>>> 45c1d50556764711f22db38a4772617686de6d73
     </>
   );
 }
