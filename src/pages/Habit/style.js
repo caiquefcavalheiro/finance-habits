bb5280a8964c06a49f3fb76a9db6639bb4a1c762
Dorival Machado <dorivalmachado@outlook.com>
2021-12-12 21:56:41 -0300

    feat(page habit): create page with selected habit information

--- src/pages/Habit/style.js
+++ src/pages/Habit/style.js
@@ -1,1 +1,67 @@
 import styled from "styled-components";
+
+export const SecondaryContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    @media screen and (min-width: 600px){
+        flex-direction: row;
+    }
+`
+
+export const CardsBox = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    .desktop{
+        display: none;
+    }
+
+    @media screen and (min-width: 600px){
+        .desktop{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            bottom: 15px;
+            right: 15px;
+        }
+    }
+`
+
+const Card = styled.div`
+    @media screen and (min-width: 600px){
+        width: 395px;
+        height: 270px;
+        border: 1px solid var(--color-dark-blue);
+        background-color: var(--color-card-blue);
+        padding: 15px;
+        position: relative;
+        border-radius: var(--radius);
+    }
+`
+
+export const CardAchieved = styled(Card)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    @media screen and (min-width: 600px){
+        align-items: flex-end;
+        margin: 10px;
+    }
+`
+export const CardInfo = styled(Card)`
+    color: var(--color-primary-blue);
+    margin-top: 20px;
+    min-height: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+    @media screen and (min-width: 600px){
+        margin: 10px;
+        font-size: 20px;
+        color: var(--color-grey-text);
+    }
+`
