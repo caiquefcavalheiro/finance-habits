8ce9a2fcdb463a89d13a9c9d3c9e7b0e06c97c62
Gabriel <foioamaral16@gmail.com>
2021-12-13 22:46:00 -0300

    feat: displayContainer

--- src/components/BoardHabits/index.jsx
+++ src/components/BoardHabits/index.jsx
@@ -1,28 +1,31 @@
 import CardHabitCard from "../../components/CardHabit";
 import CreateHabit from "../../components/CreateHabit";
-import { Container, Header, PlusButton } from "./style";
+import { PlusButton, Container } from "./style";
 import { useSignin } from "../../providers/SignIn";
 import SubHeader from "../SubHeader";
+import { DisplayContainer } from "../DisplayContainer";
 
 const BoardHabits = ({ habitModal, setHabitModal }) => {
   const { userHabits } = useSignin();
 
   return (
-    <Container>
-      <SubHeader type="Group">
+    <DisplayContainer type="row">
+      <SubHeader type="Habit">
         Meus Hábitos <PlusButton />
       </SubHeader>
       <CreateHabit habitModal={habitModal} setHabitModal={setHabitModal} />
-      {userHabits.map((habit, index) => (
-        <CardHabitCard
-          key={index}
-          title={habit.title}
-          category={habit.category}
-          difficulty={habit.difficulty}
-          frequency={habit.frequency}
-        />
-      ))}
-    </Container>
+      <Container>
+        {userHabits.map((habit, index) => (
+          <CardHabitCard
+            key={index}
+            title={habit.title}
+            category={habit.category}
+            difficulty={habit.difficulty}
+            frequency={habit.frequency}
+          />
+        ))}
+      </Container>
+    </DisplayContainer>
   );
 };
 export default BoardHabits;
--- src/components/BoardHabits/style.js
+++ src/components/BoardHabits/style.js
@@ -2,25 +2,12 @@ import styled from "styled-components";
 import { AiOutlinePlus } from "react-icons/ai";
 
 export const Container = styled.div`
-  background: #d2d2d233;
-  height: 445px;
-  width: 270px;
-  left: 50px;
-  top: 168px;
-  border-radius: 8px;
-  margin: 2rem auto;
-  padding: 1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
-  box-shadow: 0px 0px 2px 2px #00000040;
-
   @media (min-width: 600px) {
-    width: 784.2622680664062px;
-    height: auto;
-    flex-wrap: wrap;
-    flex-direction: row;
-    justify-content: flex-start;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
   }
 `;
 export const Header = styled.div`
