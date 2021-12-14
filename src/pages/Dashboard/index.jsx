1abc4b7b2bdcf03ede4980ddc106b79652f258ff
Bernardo <bernardocardosodacosta@gmail.com>
2021-12-14 18:09:12 -0300

    feat: creating a create group function

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import BoardHabits from '../../components/BoardHabits'
 import Button from "../../components/Button"
+import CreateGroup from '../../components/CreateGroup'
 import LogoutButton from '../../components/LogoutButton'
 
 function Dashboard({ authenticated, setAuthenticated }) {
 
     const [habitModal, setHabitModal] = useState(false)
+    const [createGroupModal, setCreateGroupModal] = useState(false)
     
     if (!authenticated) {
         return <Redirect to="/"/>
@@ -20,11 +22,17 @@ function Dashboard({ authenticated, setAuthenticated }) {
     const openHabitModal = () => {
         setHabitModal(true)
     }
+
+    const openGroupModal = () => {
+        setCreateGroupModal(true)
+    }
     return(
         <>
         <LogoutButton onClick={logout} >Sair</LogoutButton>
-        <Button onClick={openHabitModal} >Criar hábito</Button>
+        <Button onClick={openHabitModal}>Criar hábito</Button>
+        <Button onClick={openGroupModal}>Criar grupo</Button>
         <BoardHabits habitModal={habitModal} setHabitModal={setHabitModal} />
+        <CreateGroup createGroupModal={createGroupModal} setCreateGroupModal={setCreateGroupModal} />
         </>
     )
 }
