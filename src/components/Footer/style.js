04da65c56c98067dc98748ba0e28ac8d1c1c889b
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2021-12-16 00:40:32 -0300

    feat(fix the bug edit dont close a modal and add a effects to footer): fix bug edit and footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -26,7 +26,33 @@ export const Container = styled.div`
 
   img {
     margin-right: 15px;
-    width: 40px;
     border-radius: 50%;
+    transform-style: preserve-3d;
+    transition: all 0.75s ease;
+  }
+`;
+
+export const TagP = styled.p`
+  display: none;
+  @media screen and (max-width: 800px) {
+    display: block;
+  }
+`;
+
+export const SpecialImg = styled.img`
+  width: 40px;
+  height: 40px;
+
+  :hover {
+    transform: rotateY(360deg);
+    content: url(https://static1.purebreak.com.br/articles/9/20/76/9/@/104805-liam-payne-do-one-direction-revela-em-diapo-2.jpg);
+  }
+`;
+
+export const CommonImg = styled.img`
+  width: 40px;
+
+  :hover {
+    transform: rotateY(360deg);
   }
 `;
